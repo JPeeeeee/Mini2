1e0f4e1fe7e0ef1b346c305b4e6bf0f1810c2e8c
JPeeeeee <joaopedrodev8@gmail.com>
2023-08-23 11:12:40 -0300

    Initial Commit

---
+++ Mini2/Mini2App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini2App.swift
+//  Mini2
+//
+//  Created by João Pedro Vieira Santos on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Mini2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
