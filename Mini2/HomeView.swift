6585f98cdfe64d67fc21b4f5ed30177f8aab926d
Anne <aannesilva48@gmail.com>
2023-11-28 08:59:09 -0300

    [ADD] commit para test flight

---
+++ Mini2/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Mini2
+//
+//  Created by Anne Caroline Siqueira da Silva on 31/08/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
